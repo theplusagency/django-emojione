3da6b64d771303638a6a7c5e6d450e99dcff64e9
Rafael Ponieman <rafa@theplus.agency>
2018-04-15 19:54:31 -0300

    Safestring and custom ruleset

--- django_emojione/functions.py
+++ django_emojione/functions.py
@@ -1,8 +1,52 @@
 __author__ = "phuong"
 __date__ = "03 31 2016, 3:05 PM"
 
+import re
+
 from django.conf import settings
 from emojipy import Emoji
+from emojipy.ruleset import shortcode_replace, ascii_replace, category_replace
+from .ruleset import unicode_replace
+
+from html import escape
+
+
+class CustomEmoji(Emoji):
+    ignored_regexp = '<object[^>]*>.*?<\/object>|<span[^>]*>.*?<\/span>|<(?:object|embed|svg|img|div|span|p|a)[^>]*>'
+    unicode_regexp = "(" + '|'.join(
+        [re.escape(x.decode("utf-8")) for x in sorted(unicode_replace.keys(), key=len, reverse=True)]) + ")"
+    unicode_compiled = re.compile(ignored_regexp + "|(" + unicode_regexp + ")", re.UNICODE)
+
+    @classmethod
+    def unicode_to_image(cls, text, **kwargs):
+        css_class = kwargs.pop('css', '')
+        style = kwargs.pop('style', '')
+
+        def replace_unicode(match):
+            unicode_char = text[match.start():match.end()]
+            unicode_encoded = unicode_char.encode('utf-8')
+            if not unicode_encoded or unicode_encoded not in unicode_replace:
+                return unicode_char  # unsupported unicode char
+            shortcode = unicode_replace[unicode_encoded]
+            if cls.unicode_alt:
+                alt = unicode_char
+            else:
+                alt = shortcode
+            filename = shortcode_replace[shortcode]
+            category = category_replace[shortcode]
+
+            if cls.sprites:
+                return '<span class="emojione emojione-32-%s _%s" title="%s">%s</span>' \
+                       % (category, filename, escape(shortcode), alt)
+            else:
+                return '<img class="emojione %s" style="%s" alt="%s" src="%s"/>' % (
+                    css_class, style, alt,
+                    cls.image_png_path + filename + '.png'
+                )
+
+        text = re.sub(cls.unicode_compiled, replace_unicode, text)
+        return text
+
 
 image_type = 'png'
 if hasattr(settings, 'EMOJIPY_IMAGE_TYPE'):
@@ -30,11 +74,11 @@ def to_image(text, **kwargs):
     :param kwargs[style]: icon stylesheet
     :return: string
     """
-    return Emoji.to_image(text, **kwargs)
+    return CustomEmoji.to_image(text, **kwargs)
 
 
 def unicode_to_image(text, **kwargs):
-    return Emoji.unicode_to_image(text, **kwargs)
+    return CustomEmoji.unicode_to_image(text, **kwargs)
 
 
 def ascii_to_unicode(text, **kwargs):
--- django_emojione/templatetags/emojione.py
+++ django_emojione/templatetags/emojione.py
@@ -2,31 +2,33 @@ __author__ = "phuong"
 __date__ = "03 31 2016, 5:24 PM"
 
 from django import template
+from django.utils.safestring import mark_safe
 from emojipy import Emoji
+from django_emojione.functions import to_image as to_image_f, unicode_to_image as unicode_to_image_f
 
 register = template.Library()
 
 
 @register.simple_tag
 def to_image(value, *args, **kwargs):
-    return Emoji.to_image(value, **kwargs)
+    return mark_safe(to_image_f(value, **kwargs))
 
 
 @register.simple_tag
 def unicode_to_image(value, *args, **kwargs):
-    return Emoji.unicode_to_image(value, **kwargs)
+    return mark_safe(unicode_to_image_f(value, **kwargs))
 
 
 @register.simple_tag
 def shortcode_to_image(value, *args, **kwargs):
-    return Emoji.shortcode_to_image(value, **kwargs)
+    return mark_safe(Emoji.shortcode_to_image(value, **kwargs))
 
 
 @register.simple_tag
 def ascii_to_image(value, *args, **kwargs):
-    return Emoji.ascii_to_image(value, **kwargs)
+    return mark_safe(Emoji.ascii_to_image(value, **kwargs))
 
 
 @register.simple_tag
 def shortcode_to_unicode(value, *args, **kwargs):
-    return Emoji.ascii_to_image(value, **kwargs)
+    return mark_safe(Emoji.ascii_to_image(value, **kwargs))
